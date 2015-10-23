a6d41bb385716d1ca491cd1bf851c9c87962ee9a
jeanbroid <jeanbroid@gmail.com>
2015-10-23 12:27:49 +0200

    Update procfile

--- run.py
+++ run.py
@@ -3,9 +3,10 @@
 import os
 from iaecal import create_app
 
+print("=== Config ===")
+print("Setting: {}".format(os.environ['APP_SETTINGS']))
+print("Database url: {}".format(os.environ['DATABASE_URL']))
+app = create_app()
+
 if __name__ == "__main__":
-    print("=== Config ===")
-    print("Setting: {}".format(os.environ['APP_SETTINGS']))
-    print("Database url: {}".format(os.environ['DATABASE_URL']))
-    app = create_app()
     app.run()
