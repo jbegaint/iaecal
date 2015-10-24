fdbbc5ac99e2fca2e149d4cfd2b9bfa47fafdbdc
jeanbroid <jeanbroid@gmail.com>
2015-10-24 18:47:26 +0200

    Cleaning

--- iaecal/static/js/main.js
+++ iaecal/static/js/main.js
@@ -25,7 +25,7 @@
 
 					// Init requested data
 					$scope.serverData = {};
-				
+
 					usSpinnerService.spin('spinner-form');
 
 					$http.post('/get-url', {
@@ -33,32 +33,29 @@
 						'password': $scope.password,
 					}).
 					success(function(data) {
-						// Clean up errors
 						$scope.errors = {};
-
 						angular.forEach(data, function(value, field) {
 							$scope.serverData[field] = value;
 						});
 						$scope.showUrl = true;
 					}).
 					error(function(data) {
-						// Clean up errors
 						$scope.errors = {};
-
-						// Parse response
 						angular.forEach(data.errors, function(errors, field) {
-							if ($scope.form.hasOwnProperty(field)) {
-								// mark field invalid
-								$scope.form[field].$setValidity('server', false);
-							}
-
-							// display server error messages
-							$scope.errors[field] = errors.join(', ');
+							// mark field invalid
+							$scope.form[field].$dirty = true;
+							$scope.form[field].$setValidity('server', false);
+							$scope.errors[field] = errors.join(", ");
 						});
 					}).
 					finally(function() {
 						usSpinnerService.stop('spinner-form');
 					});
+				};
+
+				$scope.hasError = function(name) {
+					var f = $scope.form[name];
+					return f.$invalid && f.$dirty;
 				};
 			}
 	]);
