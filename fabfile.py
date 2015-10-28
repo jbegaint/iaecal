f97aa8e7f9352e854d2639c68bcc1d2a0e267f9a
jeanbroid <jeanbroid@gmail.com>
2015-10-28 12:35:26 +0100

    Add deploy capacity to fabric

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 # fabfile.py
 
 from fabric.api import *
+import os
 
 
 def initbabel():
@@ -26,3 +27,26 @@ def babel():
 def test():
     """Test the application."""
     local('python2 iaecal_tests.py')
+
+
+def deploy():
+    """Deploy the application on heroku."""
+    # maintenance on
+    local('heroku maintenance on')
+
+    # push local changes
+    local('git push heroku master')
+
+    # set environment variables
+    local('heroku config:set APP_SETTINGS:config.ProductionConfig')
+
+    secret_key = os.urandom(24)
+    local('heroku config:set SECRET_KEY:%s' % secret_key)
+
+    env_vars = ['CAL_EVENTS_URL', 'CAL_LOGIN_URL', 'MAIL_USERNAME',
+                'MAIL_PASSWORD']
+    for v in env_vars:
+        local('heroku config:set %s:%s' % (v, os.environ.get(v)))
+
+    # maintenance off
+    local('heroku maintenance off')
